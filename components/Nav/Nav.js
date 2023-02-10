5060e0213715dd583b745fb499257e6012c5a8ed
rasooly-dev <alofiabdulprogramming@gmail.com>
2023-02-10 15:46:09 -0800

    Add Nav Dropdown for Smaller Devices

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -2,7 +2,14 @@ import styles from './Nav.module.css'
 
 import { useRouter } from 'next/router'
 
+import { useState } from 'react'
+
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 const Nav = () => {
+
+    const [expanded, setExpanded] = useState(false)
 
     return (
 
@@ -13,12 +20,17 @@ const Nav = () => {
 
             <div>
 
-                <NavList items={[
+                <NavList 
+                expanded={expanded}
+                items={[
                     { name: 'Fundraisers', link: '/' },
                     { name: 'Previous Fundraisers', link: '/previous-fundraisers' },
                     { name: 'About', link: '/about' },
                     { name: 'Contact', link: '/contact' },
-                ]} />
+                ]}
+                />
+
+                <NavExpand expanded={expanded} setExpanded={setExpanded} />
 
             </div>
         </nav>
@@ -37,10 +49,15 @@ const NavTitle = ({ title }) => {
     )
 }
 
-const NavList = ({ items }) => {
+const NavList = ({ items, expanded }) => {
 
     return (
-        <ul className={styles.list}>
+        <ul 
+        className={styles.list}
+        style={{
+            height: expanded ? 'auto' : '0',
+        }}
+        >
             {items.map((item, index) => (
                 <NavItem key={index} name={item.name} link={item.link} />
             ))}
@@ -58,6 +75,16 @@ const NavItem = ({ name, link }) => {
             </li>
         )
 }
+
+const NavExpand = ({ expanded, setExpanded }) => {
+
+    return (
+        <div className={styles.expand} onClick={() => setExpanded( (expanded) => !expanded ) }>
+            <FontAwesomeIcon icon={faBars} className={`${styles.expandIcon} ${expanded ? styles.rotate : ''}`}/>
+        </div>
+    )
+}
+
 
 // This is a placeholder component that is used to keep the layout consistent
 const NavPlaceholder = () => {
