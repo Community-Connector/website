d712f5052b4332928ecbc75b93cb1804282ac46a
rasooly-dev <alofiabdulprogramming@gmail.com>
2023-02-09 23:22:44 -0800

    Add Basic Nav Bar

---
+++ components/Nav/Nav.js
@@ -0,0 +1,62 @@
+import styles from './Nav.module.css'
+
+import { useRouter } from 'next/router'
+
+const Nav = () => {
+
+    return (
+
+        <nav className={styles.nav}>
+            <div className={styles.titleWrapper}>
+                <NavTitle title="Community Connector" />
+            </div>
+
+            <div>
+
+                <NavList items={[
+                    { name: 'Fundraisers', link: '/' },
+                    { name: 'Previous Fundraisers', link: '/previous-fundraisers' },
+                    { name: 'About', link: '/about' },
+                    { name: 'Contact', link: '/contact' },
+                ]} />
+
+            </div>
+        </nav>
+
+    )
+}
+
+const NavTitle = ({ title }) => {
+
+    const router = useRouter()
+
+    return (
+        <h1 className={styles.title} onClick={ () => router.push('/') }>
+            {title}
+        </h1>
+    )
+}
+
+const NavList = ({ items }) => {
+
+    return (
+        <ul className={styles.list}>
+            {items.map((item, index) => (
+                <NavItem key={index} name={item.name} link={item.link} />
+            ))}
+        </ul>
+    )
+}
+
+const NavItem = ({ name, link }) => {
+    
+        return (
+            <li className={styles.item}>
+                <a href={link} className={styles.link}>
+                    {name}
+                </a>
+            </li>
+        )
+}
+
+export default Nav
