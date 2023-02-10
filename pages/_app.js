a5cc4f0064469832d0731d5d09888c72c92c4480
rasooly-dev <alofiabdulprogramming@gmail.com>
2023-02-10 14:46:36 -0800

    Persist Nav on All Pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,42 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
+
+import { useRouter } from 'next/router'
+
+import { useEffect, useState } from 'react'
+
+import Nav from '../components/Nav/Nav'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const router = useRouter()
+
+  const [title, setTitle] = useState('')
+
+  useEffect(() => {
+
+    const end = router.pathname.split('/').pop()
+
+    if (end == '')
+      setTitle('Fundraisers')
+    else if (end === 'previous-fundraisers')
+      setTitle('Previous Fundraisers')
+
+    else if (end === 'about')
+      setTitle('About')
+
+    else if (end === 'contact')
+      setTitle('Contact')
+
+  }, [router.pathname])
+
+  return (
+    <>
+    <Head>
+      <title>{title}</title>
+    </Head>
+      <Nav />
+      <Component {...pageProps} />
+    </>
+  )
 }
