1aaeaefd0592a852403bbacca69c8de97c17483d
rasooly-dev <alofiabdulprogramming@gmail.com>
2023-02-10 23:12:56 -0800

    Create Page Title Component (ugly af)

---
+++ components/PageTitle/PageTitle.js
@@ -0,0 +1,12 @@
+import styles from './PageTitle.module.css'
+
+const PageTitle = ({ title, fullWidth }) => {
+    return (
+        <div className={styles.titleContainer}>
+            <h1 className={styles.title}>{title}</h1>
+            <h1 className={styles.background}>{title}</h1>
+        </div>
+    )
+}
+
+export default PageTitle
