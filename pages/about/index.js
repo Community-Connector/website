ca1469cf534734433fa7404ced8e48a7d4f715f8
rasooly-dev <alofiabdulprogramming@gmail.com>
2023-02-10 23:45:23 -0800

    Begin About Page Development

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,10 +1,18 @@
 import PageTitle from "@/components/PageTitle/PageTitle"
 
+import styles from './About.module.css'
+
 const About = () => {
 
     return (
-        <div>
+        <div className="page">
             <PageTitle title="About Us" />
+
+            <main className={styles.container}>
+
+                <h1>Who are we?</h1>
+
+            </main>
         </div>
     )
 }
