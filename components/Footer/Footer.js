a5f432910f291aa8be649df23788794f43b5b423
okayyyy <zghan2005@gmail.com>
2023-02-11 21:33:58 -0800

    Make ReKompile text in footer a hyperlink

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import styles from './Footer.module.css'
 
 const Footer = () => {
@@ -5,7 +6,8 @@ const Footer = () => {
     return (
         <footer className={styles.footer} >
             <p className={styles.text}>Copyright © 2023 Community Connector</p>
-            <p className={`${styles.text} ${styles.alignRight}`}>Created by <em>ReKompile</em> for <em>Community Connector</em>. All rights reserved.</p>
+            <p className={`${styles.text} ${styles.alignRight}`}>
+                Created by <Link href="https://rekompile.org/" className={styles.link}><em>ReKompile</em></Link> for <em>Community Connector</em>. All rights reserved.</p>
         </footer>
     )
 
