1420b192bd4cf2c5b10a0a4a62c2600371feafaa
okayyyy <zghan2005@gmail.com>
2023-02-12 09:28:10 -0800

    Fix typo

--- pages/about/index.js
+++ pages/about/index.js
@@ -40,7 +40,7 @@ const About = () => {
 
                     <p className={styles.text}>
                         Founded by Bay Area high school students during the peak of COVID-19, Community Connector is a non-profit organization 
-                        that aims to to offset and provide assistance to struggling families in the community. Through Community Connector we 
+                        that aims to offset and provide assistance to struggling families in the community. Through Community Connector we 
                         have delivered groceries to people all around the Bay and have fundraised over $25k for global campaigns. We hope 
                         to continue our efforts with your help!
                     </p>
