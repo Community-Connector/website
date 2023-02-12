acbc335509a3c07515eb1aac1c883ad22a0e4932
okayyyy <zghan2005@gmail.com>
2023-02-11 22:53:39 -0800

    Add previous fundraisers page

--- pages/previous-fundraisers/index.js
+++ pages/previous-fundraisers/index.js
@@ -1,9 +1,53 @@
-import PageTitle from "@/components/PageTitle/PageTitle"
+import Image from "next/image"
+import styles from './PreviousFundraisers.module.css'
+import AfghanFlyer from "public/images/afghan-flyer.jpeg"
+import AustinStormRelief from "public/images/austin-storm-relief.png"
+import SriLankaImg from "public/images/sri-lanka-sports-fund.jpg"
+import CarePackageCampaign from "public/images/care-package-campaign.png"
+import EidGiftCard from "public/images/eid-gift-card.jpg"
+
+const FundraiserListing = ({name, children, image, image_alt, alternate_row = false}) => {
+    return (
+        <div className={`${styles.fundraiserListing} ${alternate_row ? styles.fundraiserListingOdd : styles.fundraiserListingEven}`}>
+            <div className={styles.fundraiserTextContainer}>
+                <h3 className={styles.fundraiserTitle}>{name}</h3>
+                <p className={styles.fundraiserDescription}>{children}</p>
+            </div>
+            <div className={styles.fundraiserImageContainer}>
+                <Image
+                    src={image}
+                    alt={image_alt}
+                    className={styles.fundraiserImage}
+                />
+            </div>
+        </div>
+    )
+}
 
 const PreviousFundraisers = () => {
     return (
         <div className="page">
-            <PageTitle>Previous Fundraisers</PageTitle>
+            <FundraiserListing name="Afghanistan Relief Fund" image={AfghanFlyer} image_alt="Afghanistan Relief Flyer">
+                Provided food parcels that consisted of staple foods of the region including rice, wheat, and beans that sustained 1 family for an entire month.
+                Raised $14,000, and sent 1400 food parcels in the 1st quarter of 2022.
+            </FundraiserListing>
+
+            <FundraiserListing name="Austin Storm Relief" image={AustinStormRelief} image_alt="Austin Storm Relief Flyer" alternate_row>
+                Raised $2500 in order to support victims in Austin that were hit hard by the Snow storms in February of 2021.
+                The money went towards helping families who had suffered damage to their homes.
+            </FundraiserListing>
+
+            <FundraiserListing name="Sri Lanka / Indonesia Sports Fund" image={SriLankaImg} image_alt="Sri Lanka Image">
+                Collected $2000 for sports equipment for orphanages in Sri Lanka and Indonesia in order for them to buy any sports equipment they needed.
+            </FundraiserListing>
+
+            <FundraiserListing name="Care Package Campaign" image={CarePackageCampaign} image_alt="Care Package Campaign" alternate_row>
+                Collected $4000 to make care packages for the homeless in San Francisco. We were able to make about 450 care packages.
+            </FundraiserListing>
+
+            <FundraiserListing name="Eid Gift Card" image={EidGiftCard} image_alt="Eid Gift Card">
+                In celebration of Eid al-Fitr at the end of Ramadan, we collected $1000, to make 50 $20 gift cards and distributed them to Muslim families in Oakland. 
+            </FundraiserListing>
         </div>
     )
 }
