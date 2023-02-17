c67040a01215a7d985218a6d52a6d6547ca2c966
rasooly-dev <alofiabdulprogramming@gmail.com>
2023-02-16 20:09:02 -0800

    Add Serverside Input Validation

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -4,6 +4,23 @@ export default async function handler (req, res) {
 
     const { name, email, message } = req.body;
 
+    const valid = name.length > 0 && 
+                  email.length > 0 && 
+                  message.length > 0 &&
+                  name.length <= 50 &&
+                  email.length <= 80 &&
+                  message.length <= 500 &&
+                  email.includes('@') &&
+                  email.includes('.');
+
+    if (!valid) 
+        return res.status(400).json({
+            type: "error",
+            title: "Error Sending Message",
+            message: "You have not filled out the form properly. Please provide proper responses to the given fields."
+        });
+    
+
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 465,
@@ -24,9 +41,17 @@ export default async function handler (req, res) {
 
     try {
         await transporter.sendMail(mailOptions);
-        res.status(200).json({ message: "Email sent successfully" });
+        res.status(200).json({ 
+            type: "success",
+            title: "Message Sent Successfully",
+            message: "Your message has been sent successfully, thank you. We will get back to you as soon as possible."
+        });
     } catch (error) {
-        res.status(500).json({ message: "Email failed to send" });
+        res.status(500).json({ 
+            type: "error",
+            title: "Error Sending Message",
+            message: "Something happened while sending your message. Please try again later."
+        });
     }
 
 }
